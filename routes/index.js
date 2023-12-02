3fa6892f35c15af01fb9e1827cae7017c9c0c686
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2023-12-02 22:28:48 +0300

    4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,21 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/*Страница "Преступление и наказание" Достоевский*/
+router.get('/dost', function(req, res, next) {
+  res.send("<h1>Страница \"Преступление и наказание\" Достоевский</h1>")
+});
+/*Страница "Капитал" Маркс*/
+router.get('/marks', function(req, res, next) {
+  res.send("<h1>Страница \"Капитал\" Маркс</h1>")
+});
+/*Страница "Логика" Гегель*/
+router.get('/gegel', function(req, res, next) {
+  res.send("<h1>Страница \"Логика\" Гегель</h1>")
+});
+/*Страница "Два трактата о правлении" Локк*/
+router.get('/lokk', function(req, res, next) {
+  res.send("<h1>Страница \"Два трактата о правлении\" Локк</h1>")
+});
 
 module.exports = router;
